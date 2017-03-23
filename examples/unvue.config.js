cea0ff4e23681e58b9de62f6342ceb1137d46993
zcong1993 <1005997919@qq.com>
2017-03-23 11:13:24 +0800

    tweak example 🍑

--- examples/unvue.config.js
+++ examples/unvue.config.js
@@ -3,17 +3,17 @@ const markdownLoaderPlugin = require('unvue-plugin-markdown-loader')
 module.exports = {
   plugins: [
     // add markdownLoaderPlugin with custom options
-    // you also can use it without any options, markdownLoaderPlugin()
+    // you can also use it without any options, markdownLoaderPlugin()
     markdownLoaderPlugin({
-        preprocess: function (MarkdownIt, Source) {
-          MarkdownIt.renderer.rules.table_open = function () {
-            return '<div class="table-container"><table class="table">'
-          }
-          MarkdownIt.renderer.rules.table_close = function () {
-            return '</table></div>'
-          }
-          return Source
+      preprocess(MarkdownIt, Source) {
+        MarkdownIt.renderer.rules.table_open = function () {
+          return '<div class="table-container"><table class="table">'
         }
-      })
+        MarkdownIt.renderer.rules.table_close = function () {
+          return '</table></div>'
+        }
+        return Source
+      }
+    })
   ]
 }
